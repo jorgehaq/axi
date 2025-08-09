c20dff902f1c0db650ae4f163106e76f35cad4aa
Jorge Alvarez <44485505+jorgehaq@users.noreply.github.com>
2025-08-09 17:25:46 -0500

    chore: adjust comentary

--- core/urls.py
+++ core/urls.py
@@ -2,13 +2,13 @@ from django.urls import path
 from .views import login_view, upload_view, health, upload_view, health, data_preview, data_summary
 
 urlpatterns = [
-    """LOGIN, UPLOAD, HEALTHCHECK"""
+    # LOGIN, UPLOAD, HEALTHCHECK 
     path("health/", health, name="health"),
     path("auth/login", login_view, name="login"),
     path("upload", upload_view, name="upload"),
     
 
-    """CORE ANALYTICS"""
+    # CORE ANALYTICS
     path("data/<int:id>/preview", data_preview, name="data_preview"),
     path("data/<int:id>/summary", data_summary, name="data_summary"),
 ]
