03a011676d1b3e6558d59b39e608e5d2c04f64fc
Jorge Alvarez <44485505+jorgehaq@users.noreply.github.com>
2025-09-22 00:04:39 +0000

    fix(auth): evitar conflicto de etiqueta con django.contrib.auth

--- apps/auth/apps.py
+++ apps/auth/apps.py
@@ -3,5 +3,5 @@ from django.apps import AppConfig
 
 class AuthConfig(AppConfig):
     name = "apps.auth"
+    label = "axi_auth"  # evitar conflicto con django.contrib.auth
     verbose_name = "Autenticación"
-
